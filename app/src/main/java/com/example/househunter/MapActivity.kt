01ad6ced2454bd00b45f9fda47c6ae42345481c3
Seong-A <castle.a@Castleui-Macmini.local>
2024-03-13 15:12:33 +0900

    add map filter

--- app/src/main/java/com/example/househunter/MapActivity.kt
+++ app/src/main/java/com/example/househunter/MapActivity.kt
@@ -1,7 +1,13 @@
 package com.example.househunter
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.househunter.R
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
@@ -11,9 +17,199 @@ import com.naver.maps.map.overlay.Marker
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val selectedRoomTypes = mutableSetOf<String>()
+    private var selectedFixMoney = 0
+    private var selectedMonthlyMoney = 0
+    private var selectedManagementMoney = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val roomTypes = arrayOf("원룸", "투ㆍ쓰리룸", "오피스텔", "아파트")
+
+        val roomTypeContainer = findViewById<LinearLayout>(R.id.room_type_container)
+        val FixmoneyContainer = findViewById<LinearLayout>(R.id.fix_money_container)
+        val MonthlymoneyContainer = findViewById<LinearLayout>(R.id.monthly_money_container)
+        val ManagementmoneyContainer = findViewById<LinearLayout>(R.id.management_money_container)
+
+        val btnCancel = findViewById<Button>(R.id.btn_cancel)
+        val btnApply = findViewById<Button>(R.id.btn_apply)
+
+        // 초기에는 리스트와 버튼을 숨김
+        roomTypeContainer.visibility = View.GONE
+        FixmoneyContainer.visibility = View.GONE
+        MonthlymoneyContainer.visibility = View.GONE
+        ManagementmoneyContainer.visibility = View.GONE
+        btnCancel.visibility = View.GONE
+        btnApply.visibility = View.GONE
+
+        val tvRoomType = findViewById<TextView>(R.id.room_type)
+        val tvFixMoney = findViewById<TextView>(R.id.fix_money)
+        val tvMonthlyMoney = findViewById<TextView>(R.id.monthly_money)
+        val tvManagementMoney = findViewById<TextView>(R.id.management_money)
+
+        tvRoomType.setOnClickListener {
+            if (roomTypeContainer.visibility == View.VISIBLE) {
+                roomTypeContainer.visibility = View.GONE
+                btnCancel.visibility = View.GONE
+                btnApply.visibility = View.GONE
+            } else {
+                roomTypeContainer.visibility = View.VISIBLE
+                btnCancel.visibility = View.VISIBLE
+                btnApply.visibility = View.VISIBLE
+            }
+        }
+
+        tvFixMoney.setOnClickListener {
+            if (FixmoneyContainer.visibility == View.VISIBLE) {
+                FixmoneyContainer.visibility = View.GONE
+                btnCancel.visibility = View.GONE
+                btnApply.visibility = View.GONE
+            } else {
+                FixmoneyContainer.visibility = View.VISIBLE
+                btnCancel.visibility = View.VISIBLE
+                btnApply.visibility = View.VISIBLE
+            }
+        }
+
+        tvMonthlyMoney.setOnClickListener {
+            if (MonthlymoneyContainer.visibility == View.VISIBLE) {
+                MonthlymoneyContainer.visibility = View.GONE
+                btnCancel.visibility = View.GONE
+                btnApply.visibility = View.GONE
+            } else {
+                MonthlymoneyContainer.visibility = View.VISIBLE
+                btnCancel.visibility = View.VISIBLE
+                btnApply.visibility = View.VISIBLE
+            }
+        }
+
+        tvManagementMoney.setOnClickListener {
+            if (ManagementmoneyContainer.visibility == View.VISIBLE) {
+                ManagementmoneyContainer.visibility = View.GONE
+                btnCancel.visibility = View.GONE
+                btnApply.visibility = View.GONE
+            } else {
+                ManagementmoneyContainer.visibility = View.VISIBLE
+                btnCancel.visibility = View.VISIBLE
+                btnApply.visibility = View.VISIBLE
+            }
+        }
+
+        for (roomType in roomTypes) {
+            val textView = TextView(this)
+            textView.text = roomType
+            textView.setPadding(30, 8, 30, 8)
+            textView.setBackgroundResource(R.drawable.btn_gray_rounded) // 초기 배경
+            textView.setOnClickListener {
+                // 선택된 방 종류를 집합에 추가 또는 제거
+                if (selectedRoomTypes.contains(roomType)) {
+                    selectedRoomTypes.remove(roomType)
+                    textView.setBackgroundResource(R.drawable.btn_gray_rounded) // 선택 해제
+                } else {
+                    selectedRoomTypes.add(roomType)
+                    textView.setBackgroundResource(R.drawable.btn_red_rounded) // 선택
+                }
+            }
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(0, 0, 20, 0)
+            textView.layoutParams = layoutParams
+            roomTypeContainer.addView(textView)
+        }
+
+        // 보증금 SeekBar 초기화
+        val seekBar1 = findViewById<SeekBar>(R.id.seek_bar1)
+        seekBar1.max = 100 // 최대값을 1억으로 설정
+        seekBar1.progress = 0 // 초기값을 0만원으로 설정
+        seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                selectedFixMoney = progress * 100 // 1 단위 = 100만원
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        val seekBar2 = findViewById<SeekBar>(R.id.seek_bar2)
+        seekBar2.max = 10 // 최대값을 100만원으로 설정
+        seekBar2.progress = 0 // 초기값을 0만원으로 설정
+        seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                selectedMonthlyMoney = progress * 10 // 1 단위 = 10만원
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        val seekBar3 = findViewById<SeekBar>(R.id.seek_bar3)
+        seekBar3.max = 30 // 최대값을 30만원으로 설정
+        seekBar3.progress = 0 // 초기값을 0만원으로 설정
+        seekBar3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                selectedManagementMoney = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        btnCancel.setOnClickListener {
+            // 취소 버튼 클릭 시 동작 정의
+            roomTypeContainer.visibility = View.GONE
+            FixmoneyContainer.visibility = View.GONE
+            MonthlymoneyContainer.visibility = View.GONE
+            ManagementmoneyContainer.visibility = View.GONE
+            btnCancel.visibility = View.GONE
+            btnApply.visibility = View.GONE
+        }
+
+        btnApply.setOnClickListener {
+            val selectedRoomTypeText = selectedRoomTypes.joinToString(", ")
+            tvRoomType.text = if (selectedRoomTypes.isEmpty()) "방 종류" else selectedRoomTypeText
+
+            // 보증금 값이 0보다 크면 fixmoney에 추가
+            if (selectedFixMoney > 0) {
+                val formattedDeposit = String.format("%d", selectedFixMoney)
+                val moneyTypeText = "$formattedDeposit 만원"
+                tvFixMoney.text = moneyTypeText
+            }
+
+            // 월세 값이 0보다 크면 monthly_money에 추가
+            if (selectedMonthlyMoney > 0) {
+                val formattedRent = String.format("%d", selectedMonthlyMoney)
+                val moneyTypeText = "$formattedRent 만원"
+                tvMonthlyMoney.text = moneyTypeText
+            }
+
+            // 관리비가 0보다 크면 management_money에 추가
+            if (selectedManagementMoney > 0) {
+                val formattedManage = String.format("%d", selectedManagementMoney)
+                val moneyTypeText = "$formattedManage 만원"
+                tvManagementMoney.text = moneyTypeText
+            }
+
+            roomTypeContainer.visibility = View.GONE
+            FixmoneyContainer.visibility = View.GONE
+            MonthlymoneyContainer.visibility = View.GONE
+            ManagementmoneyContainer.visibility = View.GONE
+            btnCancel.visibility = View.GONE
+            btnApply.visibility = View.GONE
+        }
+
 
         // 네이버 지도 클라이언트 ID 가져오기
         val naverClientId = getString(R.string.NAVER_CLIENT_ID)
@@ -34,13 +230,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
         // 지도에 첫 번째 마커 표시
         val marker = Marker()
-        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.position = LatLng(37.5670135, 126.9783740) // 여기에서 position 설정
         marker.map = naverMap
 
         // 지도에 두 번째 마커 표시
         val marker2 = Marker()
-        marker2.position = LatLng(37.563119, 126.969418)
+        marker2.position = LatLng(37.563119, 126.969418) // 여기에서 position 설정
         marker2.map = naverMap
     }
-
 }
