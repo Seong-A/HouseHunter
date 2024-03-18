d20ecc7ff15ebd7fd399152340bb563a9a2e0b83
minsun <sms010117@naver.com>
2024-03-18 17:17:59 +0900

    Sign Out

--- app/src/main/java/com/example/househunter/MypageActivity.kt
+++ app/src/main/java/com/example/househunter/MypageActivity.kt
@@ -39,6 +39,16 @@ class MypageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        findViewById<View>(R.id.logout).setOnClickListener{
+            firebaseAuth.signOut()
+
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
